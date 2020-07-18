cf294510a1cc077e0aef086a26918f77ba07e18c
tadatoshi satou <tdts0402@gmail.com>
2020-07-18 16:49:49 +0000

    view task

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible"
+content="IE=edge">
+        <meta name="viewport" content="width=device-
+width, initial-scale=1">
+        
+        <title>MyProfile2</title>
+    </head>
+    <body>
+        <h1>MyProfileEdit</h1>
+    </body>
+</html>
