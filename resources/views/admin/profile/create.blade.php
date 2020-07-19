25f837edf652add85afbd18f498878f7727325b0
tadatoshi satou <tdts0402@gmail.com>
2020-07-19 17:57:00 +0000

    blade task

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,5 +11,17 @@ width, initial-scale=1">
     </head>
     <body>
         <h1>MyProfileCreate</h1>
+        
+        @extends('layouts.profile')
+        @section('title', 'プロフィールの新規作成')
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>プロフィール新規作成</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection    
     </body>
 </html>
