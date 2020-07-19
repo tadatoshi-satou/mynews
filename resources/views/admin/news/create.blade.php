525fa6ef1d215109be8eb087d16adeb53c7fce79
tadatoshi satou <tdts0402@gmail.com>
2020-07-19 09:52:19 +0000

    testbranch作成

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -11,5 +11,17 @@ width, initial-scale=1">
     </head>
     <body>
         <h1>Myニュース作成画面</h1>
+        
+        @extends('layouts.admin')
+        @section('title', 'ニュースの新規作成')
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>ニュース新規作成</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection    
     </body>
 </html>
